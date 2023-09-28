d3ad704f55e89eda515a3907eb3d14af8d5c81bc
Gnanalakshmi30 <114067998+Gnanalakshmi30@users.noreply.github.com>
2023-09-28 13:31:58 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Gnanalakshmi\\Task\\meeting_scheduler" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Src\\flutter"
+  "PROJECT_DIR=C:\\Gnanalakshmi\\Task\\meeting_scheduler"
+  "FLUTTER_ROOT=C:\\Src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Gnanalakshmi\\Task\\meeting_scheduler\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Gnanalakshmi\\Task\\meeting_scheduler"
+  "FLUTTER_TARGET=C:\\Gnanalakshmi\\Task\\meeting_scheduler\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Gnanalakshmi\\Task\\meeting_scheduler\\.dart_tool\\package_config.json"
+)
